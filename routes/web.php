d9563693f81991940b156cb06ac2fa3cb62d81e6
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-20 02:29:46 -0400

    Add questions component

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', \App\Livewire\ShowThreads::class)->middleware(['auth'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
