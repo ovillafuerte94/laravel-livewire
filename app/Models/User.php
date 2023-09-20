a1be3be06ae9a5c581fa5be7800f77c7309f0b0f
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-20 02:53:21 -0400

    Set relationship of user and threads

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
