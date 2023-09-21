58790c5ae12a581695c8d662041e12e3bff3767c
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-20 22:00:01 -0400

    Add category filter

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -9,12 +9,23 @@ use App\Models\Category;
 class ShowThreads extends Component
 {
     public $search = '';
+    public $category = '';
+
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
+    }
 
     public function render()
     {
         $categories = Category::get();
         $threads = Thread::query();
         $threads->where('title', 'like', '%' . $this->search . '%');
+
+        if ($this->category) {
+            $threads->where('category_id', $this->category);
+        }
+
         $threads->withCount('replies');
         $threads->latest()->withCount('replies');
 
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -4,13 +4,22 @@
         <ul>
             @foreach ($categories as $category)
                 <li class="mb-2">
-                    <a href=""
-                        class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                    <a href="#"
+                        wire:click.prevent="filterByCategory('{{ $category->id }}')"
+                        class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize"
+                    >
                         <span class="w-2 h-2 rounded-full" style="background-color: {{ $category->color }}"></span>
                         {{ $category->name }}
                     </a>
                 </li>
             @endforeach
+                <li class="mb-2">
+                    <a href="#" wire:click.prevent="filterByCategory('')"
+                        class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                        <span class="w-2 h-2 rounded-full" style="background-color: #000"></span>
+                        All results
+                    </a>
+                </li>
         </ul>
     </div>
     <div class="w-full">
