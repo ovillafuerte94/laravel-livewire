67fff6acb051b7e4b6a1948c339f9a7055a3435e
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-23 20:57:13 -0400

    Add form fields

--- resources/views/threads/form.blade.php
+++ resources/views/threads/form.blade.php
@@ -1,3 +1,29 @@
 <div>
-    form
+    <select
+        name="category_id"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full text-white/60 text-xs mb-4 capitalize"
+    >
+        <option value="">Select category</option>
+        @foreach ($categories as $category)
+            <option
+                value="{{ $category->id }}"
+                @selected($category->id == $thread->category_id)
+            >{{ $category->name }}</option>
+        @endforeach
+    </select>
+
+    <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value="{{ $thread->title }}"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full text-white/60 text-xs mb-4"
+    >
+
+    <textarea
+        name="body"
+        rows="10"
+        placeholder="Content"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full text-white/60 text-xs mb-4"
+    >{{ $thread->body }}</textarea>
 </div>
