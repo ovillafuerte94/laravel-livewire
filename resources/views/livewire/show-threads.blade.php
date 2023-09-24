d0fe42817c30e1418268be726b004b90ca49b011
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-23 20:29:48 -0400

    Minor changes

--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -1,6 +1,6 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 flex gap-10 py-12">
     <div class="w-64">
-        <a href="" class="block w-1full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-700 text-white/90 font-bold text-xs text-center rounded-md">Ask</a>
+        <a href="" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-700 text-white/90 font-bold text-xs text-center rounded-md">Ask</a>
         <ul>
             @foreach ($categories as $category)
                 <li class="mb-2">
